136b6e2767287aaaa1ed417a7d7759162d3c77de
Michael Ward <michael@scripthungry.com>
2023-10-24 23:40:24 +0100

    Sort tailwind classes on save Fixes #1

--- mix.exs
+++ mix.exs
@@ -49,7 +49,9 @@ defmodule MichaelwardUk.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      # Custom deps
+      {:tailwind_formatter, "~> 0.4.0", only: [:dev, :test], runtime: false}
     ]
   end
 
